3d67c47ad6bb87d5421bdbafa5b85ce17633af30
Jakub Frączek <kuba.fraczek@gmail.com>
2017-02-20 20:07:39 +0100

    query with select

--- src/main/java/pl/sda/TestJBDC.java
+++ src/main/java/pl/sda/TestJBDC.java
@@ -2,24 +2,30 @@ package pl.sda;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.Date;
 
 public class TestJBDC {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		Class.forName("org.postgresql.Driver");
 		Connection conn = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/postgres", "postgres",	"postgres");
 		
-		String query = "SELECT ename FROM SDA.EMP";
+		String enameTofinde = "KING";
 		
-		Statement stmt = conn.createStatement();
-		ResultSet rs = stmt.executeQuery(query);
+		String query = "SELECT *" +" FROM SDA.EMP" + " where ename = ?";
+		
+		PreparedStatement stmt = conn.prepareStatement(query);
+		stmt.setString(1, enameTofinde);
+		ResultSet rs = stmt.executeQuery();
 		
 		int id = 0;
 		while (rs.next()){
 			String ename = rs.getString("ename");
-			System.out.println("Employee no. " + ++id + ": " + ename);
+			String job = rs.getString("job");
+			Date hireDate = rs.getDate("hiredate");
+			System.out.println("Employee no. " + ++id + ": " + ename + ", job: " + job + ", hiredate: " + hireDate);
 		}
 		
 		conn.close();
